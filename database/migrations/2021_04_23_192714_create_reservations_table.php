5af04164fef9be9c43d9f6b41abc89ad11714c0d
mariem-fatnassi <mariemfatnassi144@gmail.com>
2021-04-24 05:08:06 +0200

    first_commit

---
+++ database/migrations/2021_04_23_192714_create_reservations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('client_name',30);
+            $table->date('date_arrivee');
+            $table->integer('nbr_nuits');
+            $table->integer('nbr_chambres');
+            $table->integer('nbr_adultes');
+            $table->integer('nbr_enfants');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
---
+++ database/migrations/2021_04_24_012656_create_activites_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activites', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('Nom_activité',100);
+            $table->date('Date_activité');
+            $table->dateTime('Temps_activité');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activites');
+    }
+}

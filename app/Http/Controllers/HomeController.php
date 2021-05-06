5e199723a1b9415991d5777ff05c3e3287cbdc42
mariem-fatnassi <81833843+mariem-fatnassi@users.noreply.github.com>
2021-05-06 23:15:44 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->except('welcome');
+        $this->middleware('auth'); 
     }
 
     /**
@@ -24,9 +24,5 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
-    }
-    public function welcome()
-    {
-        return view('welcome');
     }
 }

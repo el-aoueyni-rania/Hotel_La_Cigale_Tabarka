33e43dadc5aa64a67776d2cc716f5c0bde7be2eb
el-aoueyni-rania <elaoueynirania@gmail.com>
2021-05-16 20:52:41 +0200

    verif2

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -134,8 +134,7 @@
 
   </div>
 </div>
-   <!-- Scripts -->
-   <script src="{{ asset('js/app.js') }}" defer></script>
+
 
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>

7fa5a240dd4200f936cd6afa83a83ba1d8ab9c49
eya-ghazouani <81834173+eya-ghazouani@users.noreply.github.com>
2021-06-05 05:56:52 +0200

    fontawesome

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -55,14 +55,13 @@
           </li>
           <li class="nav-item">
             <a class="nav-link" href="{{route('reservations.index')}}">
-              <span data-feather="file"></span>
+            <i class="fas fa-hotel"></i>
               Reservations
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="{{route('restaurations.index')}}">
-              <span data-feather="shopping-cart"></span>
-              Restaurations
+            <i class="fas fa-utensils"></i>              Restaurations
             </a>
           </li>
           <li class="nav-item">
@@ -73,15 +72,13 @@
           </li>
           <li class="nav-item">
             <a class="nav-link" href="{{route('services.index')}}">
-              <span data-feather="bar-chart-2"></span>
-              Services
+            <i class="fas fa-concierge-bell"></i>              Services
 
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="{{route('bienetres.index')}}">
-              <span data-feather="layers"></span>
-
+            <i class="fas fa-smile-beam"></i>
               Bien Etres
 
             </a>
@@ -95,38 +92,7 @@
           </li>
         </ul>
 
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>Saved reports</span>
-          <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
-            <span data-feather="plus-circle"></span>
-          </a>
-        </h6>
-        <ul class="nav flex-column mb-2">
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Current month
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Last quarter
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Social engagement
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              Year-end sale
-            </a>
-          </li>
-        </ul>
+      
       </div>
     </nav>
 
